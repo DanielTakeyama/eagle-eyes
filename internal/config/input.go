66293466a3e6c514d244299d5088d8b8732e594c
DanielTakeyama <daniel.martins.a.prado@gmail.com>
2025-10-26 20:31:44 -0300

    refactor(GetURL): Função quase pronta, agora falta criar uma função auxiliar para validar se a URL começa com HTTP:// ou HTTPS:// e termina com /

--- internal/config/input.go
+++ internal/config/input.go
@@ -1,6 +1,6 @@
 // Função GetURL
 // 1º - Solicita a URL do Alvo -> OK
-// 2º - Analisa se a URL esta vazia e caso esteja, retorna um erro
+// 2º - Analisa se a URL esta vazia e caso esteja, retorna um erro -> OK
 // 3º - Analisa se a URL começa com HTTP:// ou HTTPS:// e caso não comece, retorna um erro
 // 4º - Analisa se o ultimo digito da URL é "/", caso não seja, adicione
 // 5º - Retorna a URL validada
@@ -27,7 +27,7 @@ func GetURL() (*string, error) {
 		}
 
 		// Enquanto o usuário não digitar nada ele fica preso no loop
-		if URL == ""{
+		if URL == "" /*|| validateURL == False*/{
 			fmt.Println("Erro: a URL não pode estar vazia, tente novamente!")
 		} else {
 			loop = true
