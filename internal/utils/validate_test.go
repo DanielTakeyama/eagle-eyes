87d0b27229be99888d7c32a8578d0d569936b00a
DanielTakeyama <daniel.martins.a.prado@gmail.com>
2025-10-29 08:00:49 -0300

    feature(validate_testo.go): Iniciando o esqueleto de testes

---
+++ internal/utils/validate_test.go
@@ -0,0 +1,10 @@
+package utils
+
+import "testing"
+
+func TestValidateURLInputs(t *testing.T) {
+	//value := "https://www.codeflakes.com.br/"
+	//expected := "https://www.codeflakes.com.br/"
+
+
+}
