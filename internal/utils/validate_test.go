0a78c5918b7083835f5e6ca2d0a03896491356ae
DanielTakeyama <daniel.martins.a.prado@gmail.com>
2025-10-31 17:32:54 -0300

    feature(config): Função TestValidateURLInputsBatch finalizada com sucesso

--- internal/utils/validate_test.go
+++ internal/utils/validate_test.go
@@ -1,9 +1,31 @@
 package utils
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestValidateURLInputs(t *testing.T) {
-	//value := "https://www.codeflakes.com.br/"
-	//expected := "https://www.codeflakes.com.br/"
-	
+type testCase struct{
+	input string
+	expected string
+}
+
+func TestValidateURLInputsBatch(t *testing.T) {
+	urlList := []testCase{
+		{"htt://www.codeflakes.com.br/", "url inválida: precisa começar com http:// ou https://"},
+		{"htts://www.codeflakes.com.br/", "url inválida: precisa começar com http:// ou https://"},
+		{"htps://www.codeflakes.com.br/", "url inválida: precisa começar com http:// ou https://"},
+		{"https:/www.codeflakes.com.br/", "url inválida: precisa começar com http:// ou https://"},
+	}
+
+	for _, obj := range urlList{
+		_, err := ValidateURL(&obj.input)
+		if err == nil {
+			t.Errorf("Esperava um erro, mas não retornou nenhum. Entrada: %v", obj.input)
+			continue
+		}
+		if err.Error() != obj.expected {
+			t.Errorf("O erro retornado é diferente do esperado: %v", err)
+		}
+	}
+
 }
